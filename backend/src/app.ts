5a9e1bf42d3f619b7a4af365cd93851aabd2ce77
HabibMollah <habib.prodev@gmail.com>
2023-09-29 00:25:44 +0600

    create api to get all products

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -7,8 +7,23 @@ const connectionString = process.env.DEV_DATABASE_URL || '';
 const connection = mysql.createConnection(connectionString);
 connection.connect();
 
-const port = 3000;
+app.use(express.json());
 
+const baseURL = '/api/v1';
+
+// Get all grocery products
+app.get(`${baseURL}/groceries`, (_req, res) => {
+  const query = `SELECT * FROM groceries;`;
+  connection.query(query, (err, results) => {
+    if (err) {
+      res.status(500).json(err);
+    } else {
+      res.send(results);
+    }
+  });
+});
+
+const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on http:///localhost:${port}`);
 });
