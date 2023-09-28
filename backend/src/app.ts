168637459077ef0aa9cb400a59db7bbb7d8d182c
HabibMollah <habib.prodev@gmail.com>
2023-09-29 00:36:25 +0600

    create api to post a product

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -11,6 +11,33 @@ app.use(express.json());
 
 const baseURL = '/api/v1';
 
+// Create a grocery product
+app.post(`${baseURL}/groceries`, (req, res) => {
+  const { title, description, imageURL } = req.body;
+
+  // Validate inputs
+  if (!title) return res.status(400).json({ error: 'Title is required' });
+  if (!description)
+    return res.status(400).json({ error: 'Description is required' });
+  if (!imageURL)
+    return res.status(400).json({ error: 'Image url is required' });
+
+  const query = `INSERT INTO groceries (title, description, imageURL)
+                 VALUES (?, ?, ?);`;
+  const values = [title, description, imageURL];
+
+  connection.query(query, values, (err, results) => {
+    if (err) {
+      console.error(err);
+      return res
+        .status(500)
+        .json({ message: 'Could not create the product', error: err });
+    }
+    const createdProduct = { results: results, title, description, imageURL };
+    return res.status(201).json(createdProduct);
+  });
+});
+
 // Get all grocery products
 app.get(`${baseURL}/groceries`, (_req, res) => {
   const query = `SELECT * FROM groceries;`;
