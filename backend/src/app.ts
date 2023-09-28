00ecbfd519b6b8f9fb349794ed769d43db04eb62
HabibMollah <habib.prodev@gmail.com>
2023-09-29 00:21:02 +0600

    setup MySQL on planetscale and connect to it

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,11 @@
 import express from 'express';
+import mysql from 'mysql2';
+import 'dotenv/config';
 
 const app = express();
+const connectionString = process.env.DEV_DATABASE_URL || '';
+const connection = mysql.createConnection(connectionString);
+connection.connect();
 
 const port = 3000;
 
