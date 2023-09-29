c9871ece51e7bc8ae2976fa374d55488a79bbcbb
HabibMollah <habib.prodev@gmail.com>
2023-09-29 19:10:35 +0600

    make data fetching fully typesafe

--- frontend/src/requests/getData.ts
+++ frontend/src/requests/getData.ts
@@ -1,1 +1,18 @@
+import ProductType from '@/types/ProductType';
+
 export const baseURL = 'https://qwikit-groceries-backend.vercel.app/api/v1';
+
+export default async function getData() {
+  try {
+    const res = await fetch(`${baseURL}/groceries`, {
+      cache: 'no-store',
+    });
+    if (!res.ok) {
+      throw new Error(`HTTP error! Status: ${res.status}`);
+    }
+    const data: ProductType[] = await res.json();
+    return data;
+  } catch (error: any) {
+    throw new Error(`Failed to fetch groceries: ${error.message}`);
+  }
+}
