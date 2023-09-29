4aa9c1fe60f570fbae1bd6ce7316cde2859e077d
HabibMollah <habib.prodev@gmail.com>
2023-09-29 15:37:04 +0600

    Design modal form

--- frontend/src/components/ModalForm.tsx
+++ frontend/src/components/ModalForm.tsx
@@ -11,8 +11,11 @@ import {
   Checkbox,
   Input,
   Link,
+  Textarea,
 } from '@nextui-org/react';
 import { MdLock, MdMail } from 'react-icons/md';
+import { BiLink } from 'react-icons/bi';
+import { BsBoxSeam } from 'react-icons/bs';
 
 export default function ModalForm({
   children,
@@ -32,27 +35,42 @@ export default function ModalForm({
         <ModalContent>
           {(onClose) => (
             <>
-              <ModalHeader className="flex flex-col gap-1">Log in</ModalHeader>
+              <ModalHeader className="flex flex-col gap-1">
+                Add A Product
+              </ModalHeader>
               <ModalBody>
                 <Input
+                  isRequired
                   autoFocus
                   endContent={
-                    <MdMail className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                    <BsBoxSeam className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
                   }
-                  label="Email"
-                  placeholder="Enter your email"
+                  label="Title"
+                  labelPlacement="outside"
+                  placeholder="Enter product name e.g. Dairy Milk"
                   variant="bordered"
+                />
+                <Textarea
+                  isRequired
+                  isInvalid={false}
+                  variant="bordered"
+                  label="Description"
+                  labelPlacement="outside"
+                  placeholder="Enter product details e.g. Farm fresh milk from Austrailia."
+                  errorMessage=""
+                  className=""
                 />
                 <Input
+                  isRequired
                   endContent={
-                    <MdLock className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
+                    <BiLink className="text-2xl text-default-400 pointer-events-none flex-shrink-0" />
                   }
-                  label="Password"
-                  placeholder="Enter your password"
-                  type="password"
+                  label="Image URL"
+                  labelPlacement="outside"
+                  placeholder="e.g. https://images.unsplash.com/photo-1622206151226-18ca2c9ab4a1?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=3270&q=80"
                   variant="bordered"
                 />
-                <div className="flex py-2 px-1 justify-between">
+                {/* <div className="flex py-2 px-1 justify-between">
                   <Checkbox
                     classNames={{
                       label: 'text-small',
@@ -62,14 +80,14 @@ export default function ModalForm({
                   <Link color="primary" href="#" size="sm">
                     Forgot password?
                   </Link>
-                </div>
+                </div> */}
               </ModalBody>
               <ModalFooter>
                 <Button color="danger" variant="flat" onPress={onClose}>
-                  Close
+                  Cancel
                 </Button>
                 <Button color="primary" onPress={onClose}>
-                  Sign in
+                  Add
                 </Button>
               </ModalFooter>
             </>
