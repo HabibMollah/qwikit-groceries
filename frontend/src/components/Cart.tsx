781b317b92f1034ff0117cb22aa9360167820b13
HabibMollah <habib.prodev@gmail.com>
2023-09-29 14:37:12 +0600

    add badge to cart

--- frontend/src/components/Cart.tsx
+++ frontend/src/components/Cart.tsx
@@ -1,14 +1,13 @@
 import { MdOutlineLocalGroceryStore } from 'react-icons/md';
 import ModalComponent from './Modal';
+import { Badge } from '@nextui-org/badge';
 
 export default function Cart() {
   return (
     <ModalComponent className="bg-[#f1f1f2] dark:bg-[#3f3f46]">
-      <div className="flex items-center gap-2">
-        <p className="text-2xl">0</p>
-        <div className="bg-gray-600 dark:bg-gray-400 h-6 w-[2px]"></div>
+      <Badge content="5" color="primary">
         <MdOutlineLocalGroceryStore size={26} />
-      </div>
+      </Badge>
     </ModalComponent>
   );
 }
