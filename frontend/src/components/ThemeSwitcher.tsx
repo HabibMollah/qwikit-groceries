88466127c27b57c714ce9905c5eaebeb7e817304
HabibMollah <habib.prodev@gmail.com>
2023-09-29 07:56:15 +0600

    update ThemeSwitcher

---
+++ frontend/src/components/ThemeSwitcher.tsx
@@ -0,0 +1,33 @@
+'use client';
+
+import { Switch } from '@nextui-org/switch';
+import { useTheme } from 'next-themes';
+import { useEffect, useState } from 'react';
+import { TbSunHigh, TbMoon } from 'react-icons/tb';
+
+export default function ThemeSwitcher() {
+  const [mounted, setMounted] = useState(false);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  return (
+    <div>
+      <Switch
+        onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
+        defaultSelected
+        size="md"
+        color="primary"
+        thumbIcon={({ isSelected, className }) =>
+          isSelected ? (
+            <TbMoon className={className} />
+          ) : (
+            <TbSunHigh className={className} />
+          )
+        }
+      />
+    </div>
+  );
+}
